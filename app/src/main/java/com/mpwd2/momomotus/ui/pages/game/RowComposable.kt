873e553ea31abcadb76863d19395433eb10a62c4
StefanSpeterDev <stefanspeter26@gmail.com>
2022-06-08 12:01:53 +0200

    manage text input

--- app/src/main/java/com/mpwd2/momomotus/ui/pages/game/RowComposable.kt
+++ app/src/main/java/com/mpwd2/momomotus/ui/pages/game/RowComposable.kt
@@ -2,29 +2,51 @@ package com.mpwd2.momomotus.ui.pages.game
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.mpwd2.momomotus.data.entities.State
 
 @Composable
 fun RowComposable() {
 
-    var x = 0
-    while(x < 5) {
-        x++;
-        Row(
+    val viewModel: GameViewModel = hiltViewModel()
+    val state = viewModel.state.collectAsState().value // récupère valeur du state
+
+    if (state is State.Success) {
+        var x = 0
+        while (x < 5) {
+            x++;
+            Row(
+                modifier = Modifier
+                    .padding(5.dp)
+                    .border(BorderStroke(2.dp, Color.Black))
+                    .padding(5.dp)
+            ) {
+                state.data.name.forEach { _ -> InputComposable(state = state, modifier = Modifier.weight(1f).aspectRatio(1f)
+                    ) }
+            }
+        }
+    } else if (state is State.Loading) {
+        CircularProgressIndicator()
+    } else {
+        Column(
             modifier = Modifier
-                .padding(5.dp)
-                .border(BorderStroke(2.dp, Color.Black))
-                .padding(5.dp)
+                .fillMaxSize()
+                .wrapContentSize(Alignment.Center)
         ) {
-            Text(text = "J'affiche 5 row $x")
+            Text(
+                text = "Erreur api",
+                fontWeight = FontWeight.Bold
+            )
         }
     }
-
-
 }
