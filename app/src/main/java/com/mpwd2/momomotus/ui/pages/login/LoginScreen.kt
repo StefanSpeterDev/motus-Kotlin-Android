a28ec9eb6c4645a6f21f47df217d5ae6eb4cb377
One-cy <plancke.cyril@gmail.com>
2022-06-09 12:35:33 +0200

    add regex check for login

--- app/src/main/java/com/mpwd2/momomotus/ui/pages/login/LoginScreen.kt
+++ app/src/main/java/com/mpwd2/momomotus/ui/pages/login/LoginScreen.kt
@@ -1,12 +1,20 @@
 package com.mpwd2.momomotus.ui.pages.login
 
 import android.annotation.SuppressLint
+import android.text.TextUtils
+import android.util.Patterns
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -16,14 +24,30 @@ import androidx.navigation.NavController
 import com.mpwd2.momomotus.data.entities.State
 import com.mpwd2.momomotus.ui.navigation.NavigationKeys
 
+
+fun isValidEmail(target: CharSequence?): Boolean {
+    return if (TextUtils.isEmpty(target)) {
+        false
+    } else {
+        Patterns.EMAIL_ADDRESS.matcher(target).matches()
+    }
+}
+
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun LoginScreen(navController: NavController){
+fun LoginScreen(navController: NavController) {
 
     var mEmailTextFieldValue by remember { mutableStateOf(TextFieldValue("")) }
     var mPasswordTextFieldValue by remember {
         mutableStateOf(TextFieldValue())
     }
+
+    // Regex
+    val regexPassword = "[0-9A-Za-z]{6,16}".toRegex()
+    var password by rememberSaveable { mutableStateOf("") }
+    val borderColorEmail = remember { mutableStateOf(Color.Blue) }
+    val borderColorPassword = remember { mutableStateOf(Color.Blue) }
+
 
     val viewModel: LoginViewModel = hiltViewModel()
 
@@ -37,7 +61,7 @@ fun LoginScreen(navController: NavController){
 
     Scaffold(
         topBar = {
-            TopAppBar(title = { Text(text = "Connectez-vous!")})
+            TopAppBar(title = { Text(text = "Connectez-vous!") })
         }
     ) {
         Column(
@@ -45,11 +69,50 @@ fun LoginScreen(navController: NavController){
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center,
         ) {
-            Text(text = "Connectez-vous !", style = TextStyle(fontSize = 26.sp, textAlign = TextAlign.Center))
+            Text(
+                text = "Connectez-vous !",
+                style = TextStyle(fontSize = 26.sp, textAlign = TextAlign.Center)
+            )
             Box(modifier = Modifier.height(20.dp))
-            TextField(value = mEmailTextFieldValue , onValueChange = { mEmailTextFieldValue = it})
+            TextField(value = mEmailTextFieldValue,
+                modifier = Modifier.border(width = 2.dp, color = borderColorEmail.value),
+                onValueChange = { mEmailTextFieldValue = it })
             Box(modifier = Modifier.height(5.dp))
-            TextField(value = mPasswordTextFieldValue, onValueChange = {mPasswordTextFieldValue = it})
+            TextField(
+                value = mPasswordTextFieldValue,
+                modifier = Modifier.border(width = 2.dp, color = borderColorPassword.value),
+                onValueChange = { mPasswordTextFieldValue = it })
+            Box(modifier = Modifier.height(5.dp))
+            TextField(
+                value = password,
+                modifier = Modifier.border(width = 2.dp, color = borderColorPassword.value),
+                onValueChange = { password = it },
+                label = { Text("Enter password") },
+                visualTransformation = PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+            )
+            TextButton(onClick = {
+                val isEmailOK = isValidEmail(mEmailTextFieldValue.text)
+                val isPassOK = regexPassword.matches(password)
+                if (isEmailOK)
+                    borderColorEmail.value = Color.Blue
+                else
+                    borderColorEmail.value = Color.Red
+                if (!isPassOK)
+                    borderColorPassword.value = Color.Red
+                else
+                    borderColorPassword.value = Color.Blue
+
+                if (isEmailOK && isPassOK) {
+                    viewModel.login(
+                        mEmailTextFieldValue.text,
+                        mPasswordTextFieldValue.text,
+                    )
+                }
+
+            }) {
+                Text(text = "Check Regex")
+            }
             TextButton(onClick = {
                 viewModel.login(
                     mEmailTextFieldValue.text,
